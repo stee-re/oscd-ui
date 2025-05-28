6114095a715daeac13fae619c93c78fb1fb14867
stee-re <stephen.renwick@omicronenergy.com>
2025-05-28 17:15:05 +0200

    chore: release 0.0.3

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## [0.0.3](https://github.com/OMICRONEnergyOSS/oscd-ui/compare/oscd-ui-v0.0.2...oscd-ui-v0.0.3) (2025-05-28)
+
+
+### Features
+
+* make all web components scoped ([eb2aae9](https://github.com/OMICRONEnergyOSS/oscd-ui/commit/eb2aae95162482a1103501c2e692fd039e70a4fc))
---
+++ web-test-runner.config.js
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { playwrightLauncher } from "@web/test-runner-playwright";
+import { jasmineTestRunnerConfig } from "web-test-runner-jasmine";
+
+export default {
+  ...jasmineTestRunnerConfig(),
+  nodeResolve: true,
+  files: ["**/*.spec.js", "!node_modules/"],
+  browsers: [
+    playwrightLauncher({
+      product: "chromium",
+      // Exclude since Firefox errors with "Touch is not defined"
+      // product: 'firefox',
+      // Exclude since Webkit errors with "Unknown error"
+      // product: 'webkit',
+    }),
+  ],
+};
